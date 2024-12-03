cd07e71d00b45ef6f283baa99b934f36cda41744
Anthony Smith <anthonysmith80@gmail.com>
2024-12-03 17:17:28 +0000

    Fix dropdown issues with Bootstrap 5 (mode dropdown class element)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-jQuery(document).ready(function($) {
-    // Focus on first input element in dropdown when expanded (e.g., search box)
-    $(".dropdown").on("shown.bs.dropdown", function (event) {
-        $(event.target).find(':input:enabled:visible:first').focus();
-    });
+document.getElementById('dropdownMenuParent').addEventListener('shown.bs.dropdown', function () {
+  document.getElementById('searchBox').focus();
 });
